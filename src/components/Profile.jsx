3f303cf581a47431461f46963dcc2f6d913834f6
SaranaDmitriy <Dmitry2016@gmail.com>
2020-10-05 22:36:51 +0300

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"></img>
+            </div>
+            <div>Ava</div>
+            <div>My post</div>
+            <div>New post
+            <div>Post1</div>
+                <div>Post2</div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile; 
